60382debe931a538cb889f18bd10e07599b7e9b2
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-01-11 22:05:32 +0100

    Add more code to js

--- js/script.js
+++ js/script.js
@@ -67,7 +67,8 @@ function newGame() {
     setGameElements();
 
     playerNameElem.innerHTML = player.name;
-    // setGamePoints(); // Jeszcze nie mamy tej funkcji
+
+    setGamePoints();
 
     }
 }
@@ -76,4 +77,62 @@ function newGame() {
 
 function playerPick(playerPick) {
   console.log(playerPick);
+}
+
+
+// wybór komputera //
+
+function getComputerPick() {
+  var possiblePicks = ['rock', 'paper', 'scissors'];
+  return possiblePicks[Math.floor(Math.random()*3)];
+}
+
+// -- dodajemy na stronę -- //
+
+var playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick'),
+    playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult');
+
+function playerPick(playerPick) {
+  var computerPick = getComputerPick();
+
+  playerPickElem.innerHTML = playerPick;
+  computerPickElem.innerHTML = computerPick;
+
+  checkRoundWinner (playerPick, computerPick);
+}
+
+// logika gry
+
+function checkRoundWinner(playerPick, computerPick) {
+  playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+
+  var winnerIs = 'player';
+
+    if (playerPick == computerPick) {
+        winnerIs = 'noone'; // remis
+    } else if (
+        (computerPick == 'rock' &&  playerPick == 'scissors') ||
+        (computerPick == 'scissors' &&  playerPick == 'paper') ||
+        (computerPick == 'paper' &&  playerPick == 'rock')) {
+
+        winnerIs = 'computer';
+    }
+
+    if (winnerIs == 'player') {
+        playerResultElem.innerHTML = "Win!";
+        player.score++;
+    } else if (winnerIs == 'computer') {
+        computerResultElem.innerHTML = "Win!";
+        computer.score++;
+    }
+
+}
+
+function setGamePoints() {
+    playerPointsElem.innerHTML = player.score;
+    computerPointsElem.innerHTML = computer.score;
+
+    console.log();
 }
